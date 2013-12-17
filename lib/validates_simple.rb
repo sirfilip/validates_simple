9d4dd7e956553f825bb4ba302d71ba0a15e2aea9
Filip Kostovski <github.sirfilip@gmail.com>
2013-12-17 23:58:12 +0100

    Bumped a version up

--- lib/validates_simple.rb
+++ lib/validates_simple.rb
@@ -3,5 +3,5 @@ require File.expand_path('../validation/rules', __FILE__)
 require File.expand_path('../validation/validator', __FILE__)
 
 module Validation
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
 end
